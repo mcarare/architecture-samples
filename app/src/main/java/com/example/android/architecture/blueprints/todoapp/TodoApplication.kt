69fa0668f5d5adbba177a8336055577e0a348b28
mcarare <mihai.carare.dev@gmail.com>
2020-08-18 12:49:40 +0300

    Setup base class to serve as the app-level DI container.

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/TodoApplication.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/TodoApplication.kt
@@ -18,6 +18,7 @@ package com.example.android.architecture.blueprints.todoapp
 
 import android.app.Application
 import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepository
+import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 import timber.log.Timber.DebugTree
 
@@ -27,6 +28,7 @@ import timber.log.Timber.DebugTree
  *
  * Also, sets up Timber in the DEBUG BuildConfig. Read Timber's documentation for production setups.
  */
+@HiltAndroidApp
 class TodoApplication : Application() {
 
     // Depends on the flavor,
